2bc6548f7beb8554841a2aea780e2b645aa3ef41
Luiz Irber <luiz.irber@gmail.com>
2012-03-06 12:40:04 -0300

    Fixed RE for namelist groups, added key_order to yaml2nml, and a really dumb way to generate the right type in yaml2nml.

--- mom4_utils/mom4_namelist.py
+++ mom4_utils/mom4_namelist.py
@@ -16,15 +16,15 @@ def nml_decode(text):
     # dummy way to deal with comments. Need to improve it
     text = re.sub("\s*!.*\n", "\n", text)
 
-    for namelists in re.findall("&((?:.*\n)+?)/", text):
+    for namelists in re.findall("&((?:.*\n)+?)\s*/", text):
         g = re.match("\s*(?P<groupname>\w+)\n((?:.*\n)*)", namelists).groups()
         output.append("%s:\n" % g[0])
         #for p in re.findall("\s+(\w+)\s*=\s*(\.?.*\.?)\s*,?\s*(!.*)\n", g[1]):
         for p in re.findall("\s*(\w+)\s*=\s*(\.?.*\.?)\s*,?\s*\n", g[1]):
             tmp = re.sub(",$|\s*", "", p[1])
-            if tmp == ".false.":
+            if tmp == ".false." or tmp == '.FALSE.':
                 tmp = False
-            if tmp == ".true.":
+            if tmp == ".true." or tmp == '.TRUE.':
                 tmp = True
             output.append("  %s: %s\n" % (p[0], tmp))
     textout = "".join(output)
@@ -40,24 +40,50 @@ def nml_decode(text):
 #re.search(group_pattern, text, re.VERBOSE).groups()
 
 
-def yaml2nml(cfg):
+def yaml2nml(cfg, key_order=None):
     """
     """
     output = []
     keys = cfg.keys()
-    keys.sort()
-    for k in keys:
+
+    if key_order:
+        key_order = list(key_order)
+        if set(key_order) <= set(keys):
+            key_order += (set(keys) - set(key_order))
+        else:
+            raise KeyError
+    else:
+        key_order = sorted(keys)
+
+    for k in key_order:
         output.append(" &%s\n" % k)
         if cfg[k] != None:
             parameters = cfg[k].keys()
             parameters.sort()
             for kk in parameters:
-                if cfg[k][kk] is False:
-                    v = '.false.'
-                elif cfg[k][kk] is True:
-                    v = '.true.'
+                v = cfg[k][kk]
+                if v is False:
+                    v = '.FALSE.'
+                elif v is True:
+                    v = '.TRUE.'
                 else:
-                    v = cfg[k][kk]
+                    try:
+                        if abs(int(v) - float(v)) == 0:
+                            v = int(v)
+                        else:
+                            v = float(v)
+                    except ValueError:
+                        try:
+                            v = float(v)
+                        except ValueError:
+                            try:
+                                v.split(',')[1]
+                            except ValueError:
+                                v = "'%s'" % v
+                            except IndexError:
+                                v = "'%s'" % v
+                            else:
+                                v = "%s" % v
                 output.append("    %s = %s,\n" % (kk, v))
         output.append("/\n\n")
     return "".join(output)
