2624622e125b7cc126cae570d7b963e197990daa
Luiz Irber <luiz.irber@gmail.com>
2014-01-24 13:08:37 -0500

    Fix #1, bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requires = ['coards', 'numpy', 'PyYAML', 'NetCDF4', 'DateUtils']
 
 setup(
     name='mom-utils',
-    version='1.2.1',
+    version='1.2.2',
     url='https://github.com/castelao/mom-utils',
     #download_url='https://bitbucket.org/castelao/mom4-utils',
     license='PSF',
